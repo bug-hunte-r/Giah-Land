279d11a21a524dc4441204e60a5980d4f04b31a5
shayan <shayanjafarnejad75@gmail.com>
2025-03-11 15:04:38 +0330

    make responsive main site

--- src/Components/MainSite/Main.jsx
+++ src/Components/MainSite/Main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Main.css'
+import './mediaMain.css'
 import { RiTruckLine } from "react-icons/ri";
 import { SiTicktick } from "react-icons/si";
 import { IoMdCard } from "react-icons/io";
@@ -10,19 +11,19 @@ export default function Main() {
             <div className="container-shop-infos">
 
                 <div className="container-card-shop-infos">
-                    <RiTruckLine size={60} className='icons-card' />
+                    <RiTruckLine size={50} className='icons-card' />
                     <h1 className="title-card">تحویل درب منزل</h1>
                     <p className='dec-card'>با ایجاد آدرس منزل خود در پروفایل کاربری،محصول خود را درب منزل تحویل بگیرید</p>
                 </div>
 
                 <div className="container-card-shop-infos">
-                    <SiTicktick size={60} className='icons-card' />
+                    <SiTicktick size={50} className='icons-card' />
                     <h1 className="title-card">ضمانت محصول</h1>
                     <p className='dec-card'>به مدت دو هفته پس از دریافت محصول در صورت وجود مشکل میتوانید مرجوع کنید</p>
                 </div>
 
                 <div className="container-card-shop-infos">
-                    <IoMdCard size={60} className='icons-card' />
+                    <IoMdCard size={50} className='icons-card' />
                     <h1 className="title-card">پرداخت درب منزل</h1>
                     <p className='dec-card'>برای ایجاد اطمینان خاطر مشتریان،علاوه بر پرداخت آنلاین امکان پرداخت درب منزل وجود دارد</p>
                 </div>
