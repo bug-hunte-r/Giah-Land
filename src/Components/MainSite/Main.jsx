f10594ba90a963feb25d6bf89531174415e2c572
shayan <shayanjafarnejad75@gmail.com>
2025-03-11 12:21:10 +0330

    develope main site`s style

--- src/Components/MainSite/Main.jsx
+++ src/Components/MainSite/Main.jsx
@@ -7,24 +7,31 @@ import { IoMdCard } from "react-icons/io";
 export default function Main() {
     return (
         <div className='container'>
-            <div className="container-infos">
-                <div className="container-product-delivery-at-home">
-                    <RiTruckLine />
-                    <h1 className="title-delivery">تحویل درب منزل</h1>
-                    <p className='dec-delivery'>با ایجاد آدرس منزل خود در پروفایل کاربری،محصول خود را درب منزل تحویل بگیرید</p>
+            <div className="container-shop-infos">
+
+                <div className="container-card-shop-infos">
+                    <RiTruckLine size={60} className='icons-card' />
+                    <h1 className="title-card">تحویل درب منزل</h1>
+                    <p className='dec-card'>با ایجاد آدرس منزل خود در پروفایل کاربری،محصول خود را درب منزل تحویل بگیرید</p>
                 </div>
 
-                <div className="container-product-delivery-at-home">
-                    <SiTicktick />
-                    <h1 className="title-delivery">ضمانت محصول</h1>
-                    <p className='dec-delivery'>به مدت دو هفته پس از دریافت محصول در صورت وجود مشکل میتوانید مرجوع کنید</p>
+                <div className="container-card-shop-infos">
+                    <SiTicktick size={60} className='icons-card' />
+                    <h1 className="title-card">ضمانت محصول</h1>
+                    <p className='dec-card'>به مدت دو هفته پس از دریافت محصول در صورت وجود مشکل میتوانید مرجوع کنید</p>
                 </div>
 
-                <div className="container-product-delivery-at-home">
-                    <IoMdCard />
-                    <h1 className="title-delivery">پرداخت درب منزل</h1>
-                    <p className='dec-delivery'>برای ایجاد اطمینان خاطر مشتریان،علاوه بر پرداخت آنلاین امکان پرداخت درب منزل وجود دارد</p>
+                <div className="container-card-shop-infos">
+                    <IoMdCard size={60} className='icons-card' />
+                    <h1 className="title-card">پرداخت درب منزل</h1>
+                    <p className='dec-card'>برای ایجاد اطمینان خاطر مشتریان،علاوه بر پرداخت آنلاین امکان پرداخت درب منزل وجود دارد</p>
                 </div>
+
+            </div>
+
+            <div className="conatiner-main-imgs">
+                <img src="/Imgs/img-left.png" className='main-site-imgs'/>
+                <img src="/Imgs/img-right.png" className='main-site-imgs'/>
             </div>
         </div>
     )
