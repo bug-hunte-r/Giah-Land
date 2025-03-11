f82bded34b1318ccf82e7c06d1afa9d487093bc9
shayan <shayanjafarnejad75@gmail.com>
2025-03-11 21:26:00 +0330

    make static slider

--- src/Components/House-Plants/HousePlants.jsx
+++ src/Components/House-Plants/HousePlants.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import ProductCard from '../Product-Card/ProductCard'
+import './HousePlants.css'
+import { GrFormPrevious } from "react-icons/gr";
+import { MdNavigateNext } from "react-icons/md";
 
 export default function HousePlants() {
     return (
         <div className='container'>
             <h1 className='title-slider'>گیاهان آپارتمانی</h1>
-            <ProductCard />
+            <div className='container-slider'>
+                <GrFormPrevious size={30} className='slider-controler-btn' />
+
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+                        <ProductCard />
+
+                <MdNavigateNext size={30} className='slider-controler-btn' />
+            </div>
         </div>
     )
 }
