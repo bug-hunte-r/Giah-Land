39299dc53fac3cf9e327d972ee612cec7ee8e675
NooberThanYall <nooberthanyall@gmail.com>
2025-03-13 18:13:28 +0300

    Updated Socket URI

--- src/Components/Dashboard/UserDash.jsx
+++ src/Components/Dashboard/UserDash.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:4000');
+const socket = io('https://giah-land-websocket.onrender.com');
 
 function UserDashboard() {
   const [message, setMessage] = useState('');
