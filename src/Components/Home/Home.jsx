9e4c0d14ecd31fe9963ab6b8a85b21f20ff0f3d3
shayan <shayanjafarnejad75@gmail.com>
2025-03-14 02:19:52 +0330

    create House-Plants-bottom files

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,6 +3,7 @@ import Header from './Header/Header'
 import HeaderBottom from './Header-Bottom/HeaderBottom'
 import Main from './MainSite/Main'
 import HousePlants from './House-Plants/HousePlants'
+import HousePlantsBottom from './House-Plants-bottom/House-Plants-bottom'
 
 export default function Home() {
     return (
@@ -11,6 +12,7 @@ export default function Home() {
             <HeaderBottom />
             <Main />
             <HousePlants />
+            <HousePlantsBottom />
         </div>
     )
 }
---
+++ src/Components/Home/House-Plants-bottom/House-Plants-bottom.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './House-Plants-bottom.css'
+
+export default function HousePlantsBottom() {
+  return (
+    <div>House-Plants-bottom</div>
+  )
+}
