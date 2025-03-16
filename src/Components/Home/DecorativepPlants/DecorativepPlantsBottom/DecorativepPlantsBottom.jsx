03a7fd7945e3062f9ce0863c795593ba3c73adf4
shayan <shayanjafarnejad75@gmail.com>
2025-03-16 13:29:35 +0330

    make responsive for DecorativePlantsBottom

--- src/Components/Home/DecorativepPlants/DecorativepPlantsBottom/DecorativepPlantsBottom.jsx
+++ src/Components/Home/DecorativepPlants/DecorativepPlantsBottom/DecorativepPlantsBottom.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import './DecorativepPlantsBottom.css'
+import './mediaDecorativepPlantsBottom.css'
 
 export default function DecorativepPlantsBottom() {
     return (
         <div className='container'>
             <div className="container-House-Plants-bottom-cards">
-                <img src="/Imgs/img 1.png" className='cards-bottom-house' />
-                <img src="/Imgs/img 2.png" className='cards-bottom-house' />
-                <img src="/Imgs/img 3.png" className='cards-bottom-house' />
-                <img src="/Imgs/img 4.png" className='cards-bottom-house' />
+                <img src="/Imgs/img-decor 4.png" className='cards-bottom-house' />
+                <img src="/Imgs/img-decor 3.png" className='cards-bottom-house' />
+                <img src="/Imgs/img-decor 2.png" className='cards-bottom-house' />
+                <img src="/Imgs/img-decor 1.png" className='cards-bottom-house' />
             </div>
         </div>
     )
