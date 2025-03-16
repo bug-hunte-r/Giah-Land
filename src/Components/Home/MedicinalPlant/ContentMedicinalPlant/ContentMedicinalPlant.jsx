2a24ce15c84fdc57e2b843dab0de1f180821b98d
shayan <shayanjafarnejad75@gmail.com>
2025-03-16 10:54:29 +0330

    make responsive MedicinalPlants section

--- src/Components/Home/MedicinalPlant/ContentMedicinalPlant/ContentMedicinalPlant.jsx
+++ src/Components/Home/MedicinalPlant/ContentMedicinalPlant/ContentMedicinalPlant.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import './ContentMedicinalPlant.css'
+import '../mediaMedicinalPlant.css'
 import { RiPlantFill } from "react-icons/ri";
 
 export default function ContentMedicinalPlant(props) {
     return (
         <>
             <div className="container-main-medicinal-plant">
-                <div className="container-title-and-desc">
-                    <h1 className="title-medicinal-plant">{props.title}</h1>
-                    <p className="desc-medicinal-plant">{props.desc}</p>
-                </div>
-                <i> <RiPlantFill className='icon-medicinal-plant' size={50} /> </i>
+                <RiPlantFill className='icon-medicinal-plant' size={50} />
+                <h1 className="title-medicinal-plant">{props.title}</h1>
+                <p className="desc-medicinal-plant">{props.desc}</p>
             </div>
         </>
     )
