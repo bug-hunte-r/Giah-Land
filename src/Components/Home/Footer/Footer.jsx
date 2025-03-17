737dbc6af36853ba3b4560aea6ab168ff57e36cd
shayan <shayanjafarnejad75@gmail.com>
2025-03-17 16:07:43 +0330

    set styles for footer

--- src/Components/Home/Footer/Footer.jsx
+++ src/Components/Home/Footer/Footer.jsx
@@ -9,44 +9,46 @@ export default function Footer() {
     return (
         <div className='container'>
             <div className="container-footer-sides">
+
+                <div className="side-left-footer">
+
+                    <div className="gift-plants-footer">
+                        <h3 className="title-side-left-footer">کادویی</h3>
+                        <p className="link-footer">آنتوریوم</p>
+                        <p className="link-footer">بونسای</p>
+                        <p className="link-footer">بنت قنسول</p>
+                    </div>
+
+                    <div className="decor-plants-footer">
+                        <h3 className="title-side-left-footer">تزئینی</h3>
+                        <p className="link-footer">پشم</p>
+                        <p className="link-footer">کراسولا</p>
+                        <p className="link-footer">کراسولا حرفه ای</p>
+                    </div>
+
+                    <div className="house-plants-footer">
+                        <h3 className="title-side-left-footer">آپارتمانی</h3>
+                        <p className="link-footer">بابا آدم</p>
+                        <p className="link-footer">یوکا</p>
+                        <p className="link-footer">سانسوریا</p>
+                    </div>
+
+                </div>
+
                 <div className="side-right-footer">
                     <h1 className='title-footer'>گیاه لند</h1>
                     <p className="desc-footer">گیاه لند سعی بر این دارد با ارائه خدمات نوین در حوزه فروش گیاهان باعث راحتی
                         شما در خرید انواع گیاه شود.تنوع گیاهان و همچنین ایجاد بستری مناسب برای مشاوره با گیاه پزشک از دیگر مزیت های گیاه لند میباشد.</p>
                     <div className="container-bottom-right-footer">
                         <h1 className="phone-number">تلفن پشتیبانی:  0212۴44</h1>
-                        <div className="social-icons">
-                            < FaInstagram />
-                            <PiTelegramLogo />
-                            <CiLinkedin />
+                        <div className="container-social-icons">
+                            < FaInstagram size={25} className='icons-footer'/>
+                            <PiTelegramLogo size={25} className='icons-footer'/>
+                            <CiLinkedin size={25} className='icons-footer'/>
                         </div>
                     </div>
                 </div>
 
-                <div className="side-left-footer">
-
-                    <div className="house-plants-footer">
-                        <h3 className="title-house-plants-footer">آپارتمانی</h3>
-                        <p className="link-house-plants-footer">بابا آدم</p>
-                        <p className="link-house-plants-footer">یوکا</p>
-                        <p className="link-house-plants-footer">سانسوریا</p>
-                    </div>
-
-                    <div className="decor-plants-footer">
-                        <h3 className="title-decor-plants-footer">تزئینی</h3>
-                        <p className="link-decor-plants-footer">پشم</p>
-                        <p className="link-decor-plants-footer">کراسولا</p>
-                        <p className="link-decor-plants-footer">کراسولا حرفه ای</p>
-                    </div>
-
-                    <div className="gift-plants-footer">
-                        <h3 className="title-gift-plants-footer">کادویی</h3>
-                        <p className="link-gift-plants-footer">آنتوریوم</p>
-                        <p className="link-gift-plants-footer">بونسای</p>
-                        <p className="link-gift-plants-footer">بنت قنسول</p>
-                    </div>
-
-                </div>
             </div>
         </div>
     )
