a2226fb663fa7638ba0427cee1ac721202be6847
shayan <shayanjafarnejad75@gmail.com>
2025-03-17 15:03:46 +0330

    create giftPlants files

---
+++ src/Components/Home/GiftPlants/GiftPlants.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import '/GiftPlants.css'
+import '/mediaGiftPlants.css'
+
+export default function GiftPlants() {
+  return (
+    <div>GiftPlants</div>
+  )
+}
---
+++ src/Components/Home/GiftPlants/product-card/product-card.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import '/product-card.css'
+
+export default function productCard() {
+  return (
+    <div></div>
+  )
+}
