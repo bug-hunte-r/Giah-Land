9f3ab29280552796d966573fc6f5d02039e7dd06
shayan <shayanjafarnejad75@gmail.com>
2025-03-20 09:50:06 +0330

    develope counselings html templetes

--- src/Components/Counseling/Counseling.jsx
+++ src/Components/Counseling/Counseling.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 import './Counseling.css'
 import './mediaCounseling.css'
 import SidebarDash from '../SidebarDash/SidebarDash'
+import { LuCopyPlus } from "react-icons/lu";
+
 
 export default function Counseling() {
   return (
     <div className='container'>
         <SidebarDash />
+
+        <div className="container-counseling">
+            <h3 className="title-counseling">مشاوره با گیاه پزشک</h3>
+            <div className="box-add-img-counseling">
+                <LuCopyPlus />
+                آپلود تصویر گیاه
+            </div>
+
+            <input className='input-ask-problem-counseling' placeholder='توضیح درباره مشکل گیاه'></input>
+
+            <button className='btn-send-problems'>ارسال</button>
+        </div>
     </div>
   )
 }
