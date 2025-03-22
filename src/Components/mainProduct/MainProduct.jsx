481562ca7ac39731b4e68f5afcd2c43754fc45d2
shayan <shayanjafarnejad75@gmail.com>
2025-03-22 13:28:43 +0330

    set styels for MainProduct

--- src/Components/mainProduct/MainProduct.jsx
+++ src/Components/mainProduct/MainProduct.jsx
@@ -11,14 +11,22 @@ export default function MainProduct() {
         <div className='container'>
             <div className="container-mainProduct">
 
-                <div className="container-main-product-img">
-                    <img src="/Imgs/Frame 11.png" className='main-product-img'/>
+                <div className="container-product-price-and-rate">
+                    <p className="title-seller">فروشنده</p>
+                    <p className="seller"> <abbr style={{color: '#396F4B'}} >عالی</abbr>فلاور گاردن | عملکرد <BsShopWindow size={17} /></p>
+                    <p className="rate">۴.۶</p>
+                    <div className="container-price">
+                        <p className="price">۵۶۰/۰۰۰ تومان</p>
+                        <p className="title-price">:قیمت</p>
+                    </div>
+                    <button className="add-product-btn">افزودن به سبد خرید</button>
                 </div>
 
                 <div className="container-main-products-infos">
                     <p className="title-main-product">نهال و گیاهان آپارتمانی</p>
-                    <h4 className="name-main-product">گیاه طبیعی یوکا</h4>
-                    <h4 className="title-infos"></h4>
+                    <h4 className="name-main-product">گیاه طبیعی بابا آدم</h4>
+                    <h4 className="title-infos">ویژگی ها</h4>
+
                     <div className="conatiner-ifos-boxes">
                         <div className="boxes-infos">
                             <p className="title-box">جنس گلدان</p>
@@ -36,24 +44,18 @@ export default function MainProduct() {
                             <p className="title-box">ابعاد</p>
                             <p className="infos-box">۲۵۰x۲۵۰x۸۰۰</p>
                         </div>
+
+                    <button className="btn-see-all-infos">مشاهده همه ویژگی ها</button>
                         <div className="boxes-infos">
                             <p className="title-box">وضعیت نسبت به آفتاب</p>
                             <p className="infos-box">آفتاب دوست</p>
                         </div>
-
-                        <button className="btn-see-all-infos">مشاهده همه ویژگی ها</button>
                     </div>
+                    
                 </div>
 
-                <div className="container-product-price-and-rate">
-                    <p className="title-seller">فروشنده</p>
-                    <p className="seller">فلاور گاردن | عملکرد عالی <BsShopWindow /></p>
-                    <p className="rate">۴.۶</p>
-                    <div className="container-price">
-                        <p className="title-price">قیمت</p>
-                        <p className="price">۵۶۰/۰۰۰ تومان</p>
-                    </div>
-                    <button className="add-product-btn">افزودن به سبد خرید</button>
+                <div className="container-main-product-img">
+                    <img src="/Imgs/Frame 11.png" className='main-product-img' />
                 </div>
             </div>
         </div>
