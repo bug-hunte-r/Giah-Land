e64c8a166020cc8cb21f793064b3506001fddb76
shayan <shayanjafarnejad75@gmail.com>
2025-03-24 19:27:59 +0330

    make 404 page for website

--- src/Components/Page404/Page404.jsx
+++ src/Components/Page404/Page404.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import './Page404.css'
+import './mediaPage404.css'
+import { Link } from 'react-router-dom'
 
 export default function Page404() {
-  return (
-    <div>404</div>
-  )
+    return (
+        <div className='container'>
+            <div className="container-404">
+                <img src="/Imgs/404.png" className='img-404' />
+                <h3 className="title-page-404">!صفحه مورد نظر شما یافت نشد</h3>
+                <Link to={'/'}> <button className='btn-return-to-home-page'>بازگشت به صفحه اصلی</button> </Link>
+            </div>
+        </div>
+    )
 }
