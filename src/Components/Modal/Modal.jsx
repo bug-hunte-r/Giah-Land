4a86659c1c40b0985d164842ad372e7cd93d4e60
shayan <shayanjafarnejad75@gmail.com>
2025-03-24 18:57:21 +0330

    make responsive the cart page

--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Modal.css'
+import './mediaModal.css'
 
 export default function Modal() {
   return (
-    <div className='container-modal'>
-        <h3 className="title-modal">عملیات با موفقیت انجام شد</h3>
-        <button className="btn-close-modal">تمام</button>
+    <div className='modal'>
+      <h3 className="title-modal">عملیات با موفقیت انجام شد</h3>
+      <button className="btn-close-modal">تمام</button>
     </div>
   )
 }
