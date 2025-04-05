873d901e64bd5fa39598fa1bd7ed7934ef396f44
shayan <shayanjafarnejad75@gmail.com>
2025-04-05 12:57:14 +0330

    fixe the sign up components bug

--- src/Components/SignUp/SignUp.jsx
+++ src/Components/SignUp/SignUp.jsx
@@ -36,9 +36,8 @@ export default function SignUp() {
       .then((data) => {
         const expirationDate = new Date();
         expirationDate.setDate(expirationDate.getDate() + 40);
-        document.cookie = `session=${
-          data.token
-        }; path=/; Secure; SameSite=Strict; Expires=${expirationDate.toUTCString()};`;
+        document.cookie = `session=${data.token
+          }; path=/; Secure; SameSite=Strict; Expires=${expirationDate.toUTCString()};`;
 
         redirect('/')
       });
@@ -48,40 +47,6 @@ export default function SignUp() {
     console.log(allUsers);
   }, []);
 
-<<<<<<< HEAD
-
-    return (
-        <div className='container'>
-
-            <form className='form-sign-up'>
-                <div className="container-img-sign-up">
-                    <img src="/Imgs/img-form.png" className='img-sign-up' />
-                </div>
-
-                <div className="container-inputs">
-                    <h3 className="title-form">ثبت نام</h3>
-
-                    <input type="text" className="inputs-login" placeholder='نام' value={name} onChange={(event) => setName(event.target.value)} />
-                    <FaRegUser size={20} className='icon-input-login' />
-
-                    <input type="text" className="inputs-login" placeholder='نام خانوادگی' value={lastName} onChange={(event) => setLastName(event.target.value)} />
-                    <FaUserTie size={20} className='icon-input-login' />
-
-                    <input type="text" className="inputs-sign-up" placeholder='شماره موبایل' value={[phonNumber]} onChange={(event) => setPhonNumber(event.target.value)} />
-                    <IoPhonePortraitOutline size={20} className='icon-input-sign-up' />
-
-                    <input type="email" className="inputs-sign-up" placeholder='ایمیل' value={gmail} onChange={(event) => setGmail(event.target.value)} />
-                    <MdOutlineMailOutline size={20} className='icon-input-sign-up' />
-
-                    <input type="password" className="inputs-sign-up" placeholder='تعریف رمز عبور' value={password} onChange={(event) => setPassword(event.target.value)} />
-                    <MdKey size={20} className='icon-input-sign-up' />
-
-                    <button className='btn-sign-up' onClick={SignUpUserHandler}>ثبت نام</button>
-
-                    <p className="link-go-to-login">حساب کاربری دارید؟ <Link to={'/Login'} className='Link-go-to-sign-in'>ورود</Link> کنید</p>
-                </div>
-            </form>
-=======
   return (
     <div className="container">
       <form className="form-sign-up">
@@ -90,7 +55,6 @@ export default function SignUp() {
             src="https://s3-alpha-sig.figma.com/img/7ec0/84a0/ec53dd269202a323721895c584b5cbc4?Expires=1743379200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=CWPvIoUEZH-51IJbu-wYCiATnkiide-B~tNwd3Lu0XRKJ1ujJvK7Qm5UkkdoBeMFgROVKq0QjX7G4d7avXDdJldMQwAOaj0wTLuIUgPKT59bfL2qEamNP20udFvzl5CbAh3fIf9ZRkhS0aeXEMyvrQrZtRoAFD5OhXgHFfUTtHJGTWO5QmthZGNm7RxZeEsEn755ZvNwOiiP-iysWKKMAuWf70aJoaRFomLlDGLNuUF6r52wCZIgkbyYmEaN2oWUw7BmW54WPSxnsW94MF5CXzuPNNFL39JQayktAg2spT7y5LWoMWrrP9KDKkzvARrWd-tXlgHknnRpFkIha7Buaw__"
             className="img-sign-up"
           />
->>>>>>> 5cd21f89eca913d7855abf0a87930b3c1a91188a
         </div>
 
         <div className="container-inputs">
@@ -155,5 +119,5 @@ export default function SignUp() {
         </div>
       </form>
     </div>
-  );
+  )
 }
